8bf97507b10e509757f5a927895277ba0316b7cf
pierrialexandervidmar <pierri@hotmail.com.br>
2024-01-23 20:20:06 -0300

    Instruções de uso

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+"""
+Nome do Script: main.py
+Autor: Pierri Alexander Vidmar
+Data de Criação: 2024-01-01
+Versão: 1.0.0
+
+Descrição:
+    Este script gera um relatório de entregas com base em dados fornecidos em um arquivo CSV.
+    Calcula a média de dias de entrega por estado, o total de entregas dentro do prazo, o estado
+    com o maior percentual de atraso, a cidade com o maior percentual de atraso e a percentagem de
+    qualidade de entrega.
+
+Dependências:
+    - pandas
+    - reportlab
+
+Uso:
+    Certifique-se de ter os arquivos 'consulta.csv' e 'main.py' no mesmo diretório.
+    Execute o script para gerar um arquivo PDF contendo o relatório de entregas.
+
+Exemplo:
+    python main.py
+
+"""
 import pandas as pd
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
